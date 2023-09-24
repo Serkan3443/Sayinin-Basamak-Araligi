19c0f06dee13a9a15fb7547e4a998b362631896e
Serkan <serkankabadayiogullari8233@gmail.com>
2023-09-24 19:45:14 +0300

    Sayinin Basamak Araligi
    Girilen sayının rakamlarının basamak aralığını bulan c# kodu

---
+++ SAYİNİN RAKAMLARİNİN BASAMAK DEGERLERİNİ BULMAK/SAYİNİN RAKAMLARİNİN BASAMAK DEGERLERİNİ BULMAK/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SAYİNİN_RAKAMLARİNİN_BASAMAK_DEGERLERİNİ_BULMAK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 basamaklı bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int yuzeayir;
+            yuzeayir = sayi / 100;
+
+            Console.WriteLine("yüzler basamağındaki sayı: " + yuzeayir);
+
+
+            int onayir;
+            sayi = sayi - (yuzeayir * 100);
+            onayir = sayi / 10;
+
+            Console.WriteLine("onlar basamağındaki sayı: " + onayir);
+
+            int bireayir;
+            sayi = sayi - (onayir * 10);
+            bireayir = sayi;
+
+            Console.WriteLine("birler basamağındaki sayı: " + bireayir);
+
+
+            Console.Read();
+        }
+    }
+}
